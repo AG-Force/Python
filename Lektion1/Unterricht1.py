2e1def0198a298605c1514b0ed7289ff9999f860
AG <85740749+AG-Force@users.noreply.github.com>
2022-08-30 12:15:12 +0200

    Add files via upload

---
+++ Lektion1/Unterricht1.py
@@ -0,0 +1,10 @@
+print("Hallo Welt");  print("Moin Chef")  # wenn man zweimal Befhele in einer Zeile schreiben will, muss ; benutzen
+
+print("Ich liebe es zu programmieren") # in Python darf man vor dem Befehl kein leerzeichen nutzen
+
+
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// außer hier
+
+
+if True:
+    print("richtig") # in dem Fall darf man Leerzeichen nutzen
